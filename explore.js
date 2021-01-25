bdace6c3416a94b42468e63bcd71b7f087ee6c5a
kaleb <kalebclement19@gmail.com>
2021-01-25 17:12:35 +0700

    new

--- explore.js
+++ explore.js
@@ -17,12 +17,21 @@ app.post('/explore/input/:City', (req,res) => {
     var reff = dbrealtime.ref('Explore').child('City').child(city);
     var key = reff.push().key;
     var inputreff = reff.child(key);
+    
+    var placename = req.body.place_name;
+    var openinghours = req.body.opening_hours;
+    var locationdetail = req.body.location_detail;
+    var priceadults = req.body.priceadults;
+    var pricekids = req.body.pricekids;
+    var description = req.body.description;
+    var price = {adults: priceadults, kids: pricekids};
     let data = {
         place_uid : key,
-        place_name: "The Great Asia Africa",
-        price : 340000,
-        opening_hours: "09.00 - 18.00",
-        details: "Looking for a quick escape without having to leave the country ? Come visit the great asia afrika where you can exploce 10 countries in 1 place."
+        place_name: placename,
+        location_detail : locationdetail,
+        price : price,
+        opening_hours: openinghours,
+        description: description
     }
     
     inputreff.set(data).then( ()=> {
